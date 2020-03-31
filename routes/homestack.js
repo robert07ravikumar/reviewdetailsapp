b416fcd3b968088958452ac4d778253ca0181160
Dell\Mabel <robert07ravikumar@gmail.com>
2020-03-31 23:23:20 +0800

    React Native Tutorial #23 - Navigation Options

--- routes/homestack.js
+++ routes/homestack.js
@@ -5,12 +5,26 @@ import ReviewDetails from '../screens/reviewDetails'
 
 const screens = {
     Home : {
-        screen: Home
+        screen: Home,
+        navigationOptions:{
+            title: 'Game Zone'
+        }
     },
     ReviewDetails : {
         screen: ReviewDetails
+        
     }
 }
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens,{
+    defaultNavigationOptions:{
+        headerTintColor : '#444',
+        headerStyle:{
+            backgroundColor : '#eee',
+            height:60,
+            
+        
+        }
+    }
+});
 
 export default createAppContainer(HomeStack);
