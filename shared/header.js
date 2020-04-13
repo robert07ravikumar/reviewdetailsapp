208e9439548d955a897eab2044c24ad45b6e896c
Dell\Mabel <robert07ravikumar@gmail.com>
2020-04-13 23:55:03 +0800

    React Native Tutorial #28 - Background Images

--- shared/header.js
+++ shared/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet,Text,View } from 'react-native';
+import { StyleSheet,Text,View,Image,ImageBackground} from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Header({navigation,title}) {
@@ -10,18 +10,19 @@ export default function Header({navigation,title}) {
     }
 
     return(
-        <View style={styles.header}>
+        <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
             <MaterialIcons name='menu' onPress={openMenu} style={styles.icon}/>
-            <View>
+            <View style={styles.headerTitle}>
+                <Image source={require('../assets/heart_logo.png')} style={styles.headerImage}/>
                 <Text style={styles.headerText}>{title}</Text>
             </View>
-        </View>
+        </ImageBackground>
     )
 }
 
 const styles= StyleSheet.create({
     header:{
-        width:'100%',
+        width:330,
         height:'100%',
         flexDirection : 'row',
         alignItems : 'center',
@@ -38,5 +39,15 @@ const styles= StyleSheet.create({
     icon :{
         position : 'absolute',
         left: 16
+    },
+    headerImage :{
+        //margin
+        height: 26,
+        width :26,
+        left: 26,
+        marginHorizontal:10
+    },
+    headerTitle :{
+        flexDirection:'row'
     }
 })
