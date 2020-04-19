b44168067023e9fa89eda6aaf727ce1fec4956c8
Dell\Mabel <robert07ravikumar@gmail.com>
2020-04-19 08:00:48 +0800

    chapter 29

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
-import {StyleSheet , View , Text , Button ,TouchableOpacity} from 'react-native' ;
+import {StyleSheet , View , Text , Button ,TouchableOpacity ,Modal} from 'react-native' ;
 import { globalStyles } from '../styles/global'
 import { FlatList } from 'react-native-gesture-handler';
 import Card from '../shared/card';
+import { MaterialIcons } from '@expo/vector-icons'
 
 export default function Home({navigation}){
-
+    const [modalOpen,setModalOpen] = useState(false); 
     const [reviews,setReviews] = useState([
         {title : ' ananconda',rating:5,body:'snake',key: '1'},
         {title : ' Jurassic park',rating:3,body:'dinosaur',key: '2'},
@@ -14,6 +15,16 @@ export default function Home({navigation}){
 
     return (
         <View style={globalStyles.container}>
+
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={StyleSheet.modalContent}>
+                    <MaterialIcons name='close' size={24} onPress={() => setModalOpen(false)}
+                        style={{...styles.modalToggle,...styles.modalClose}}/>
+                    <Text>Hello from the Modal</Text>
+                    </View>
+            </Modal>
+            <MaterialIcons name='add' size={24} onPress={() => setModalOpen(true)}
+                style={styles.modalToggle}/>
             <FlatList
                 data = {reviews}
                 renderItem={({item}) => (
@@ -28,3 +39,18 @@ export default function Home({navigation}){
         
     )
 }
+
+const styles= StyleSheet.create({
+    modalToggle:{
+        marginBottom:10,
+        borderWidth:1,
+        borderColor:'#f2f2f2',
+        padding:10,
+        borderRadius:10,
+        alignSelf:'center'
+    },
+    modalClose:{
+        marginTop:20,
+        marginBottom:0
+    }
+})
